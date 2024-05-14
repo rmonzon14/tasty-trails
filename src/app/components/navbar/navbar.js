0fbed1d8934e556aa141cfa93430b0ca9b4606c7
Red Monzon <redkmonzon30@gmail.com>
2024-05-14 14:45:07 -0500

    Add navbar.js

---
+++ src/app/components/navbar/navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <p>Tasty Trails</p>
+      <ul>
+        <li>Home</li>
+        <li>Recipes</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
